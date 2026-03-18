ad829bce0413dbcaf7da8a1bdfe539d5a7581fda
miniblack39 <251557121+miniblack39@users.noreply.github.com>
2026-03-18 12:07:15 +0900

    fix: fix analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-    <Analytics />
-  </StrictMode>,
-);
+async function renderApp() {
+  const { Analytics } = await import("@vercel/analytics/react");
+
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+      <Analytics />
+    </StrictMode>,
+  );
+}
+
+renderApp();
