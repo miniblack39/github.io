8419fc0ce41ee817a7809e59d6dc11180231b1c1
miniblack39 <251557121+miniblack39@users.noreply.github.com>
2026-03-18 12:10:00 +0900

    fix: delete analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
-async function renderApp() {
-  const { Analytics } = await import("@vercel/analytics/react");
-
-  createRoot(document.getElementById("root")).render(
-    <StrictMode>
-      <App />
-      <Analytics />
-    </StrictMode>,
-  );
-}
-
-renderApp();
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
